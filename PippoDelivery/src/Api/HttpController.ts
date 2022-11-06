634804f081351ef90bc2e0bd253a9b187b219f90
Enxhi <enxhi.minaj@gmail.com>
2022-11-06 06:18:30 +0200

    fix cors

--- PippoDelivery/src/Api/HttpController.ts
+++ PippoDelivery/src/Api/HttpController.ts
@@ -1,108 +1,138 @@
 import express, { NextFunction, Response, Request } from 'express';
 import fs from 'fs';
-import { Config } from '../Config';
 import { Routes } from './Routes';
+import cors from 'cors';
+
 /**
  * Http controller
  * Responsible of handling HTTP communication with the service
  */
 export class HttpController {
-    private _shutdown: boolean = false;
+	private _shutdown: boolean = false;
 
-    shutdown() {
-        this._shutdown = true;
-    }
+	shutdown() {
+		this._shutdown = true;
+	}
 
-    /**
-     * Middleware to enable CORS support for API
-     */
-    corsMiddleware(req: Request, res: Response, next: NextFunction) {
-        res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-    }
+	/**
+	 * Middleware to enable CORS support for API
+	 */
+	corsMiddleware(req: Request, res: Response, next: NextFunction) {
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'GET,HEAD,PUT,PATCH,POST,DELETE'
+		);
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		next();
+	}
 
-    /**
-     * Middleware to prevent new request on shutdown state
-     */
-    shutdownMiddleware = (req: Request, res: Response, next: NextFunction) => {
-        if (!this._shutdown) {
-            next();
-        } else {
-            res.set('Connection', 'close');
-            res.status(503).send({ error: 'shutdown', description: 'Server is closing down' });
-        }
-    };
+	/**
+	 * Middleware to prevent new request on shutdown state
+	 */
+	shutdownMiddleware = (req: Request, res: Response, next: NextFunction) => {
+		if (!this._shutdown) {
+			next();
+		} else {
+			res.set('Connection', 'close');
+			res.status(503).send({
+				error: 'shutdown',
+				description: 'Server is closing down',
+			});
+		}
+	};
 
-    /**
-     * Middleware for API key check
-     */
-    apiKeyMiddleware(key: string) {
-        return (req: Request, res: Response, next: NextFunction) => {
-            const apiKey = req.header('X-Api-Key');
-            if (apiKey && key === apiKey) {
-                next();
-            } else {
-                res.status(401).send({ error: 'unauthorized', description: 'Unauthorized API request' });
-            }
-        };
-    }
+	/**
+	 * Middleware for API key check
+	 */
+	apiKeyMiddleware(key: string) {
+		return (req: Request, res: Response, next: NextFunction) => {
+			const apiKey = req.header('X-Api-Key');
+			if (apiKey && key === apiKey) {
+				next();
+			} else {
+				res.status(401).send({
+					error: 'unauthorized',
+					description: 'Unauthorized API request',
+				});
+			}
+		};
+	}
 
-    /**
-     * Handle requests to API root
-     */
-    getRoot(req: Request, res: Response) {
-        res.send({
-            name: 'Wolt Service',
-        });
-    }
+	/**
+	 * Handle requests to API root
+	 */
+	getRoot(req: Request, res: Response) {
+		res.send({
+			name: 'Wolt Service',
+		});
+	}
 
-    /**
-     * Handle requests to /doc
-     */
-    getDoc(req: Request, res: Response) {
-        fs.readFile('openapi.yaml', 'utf8', (err: NodeJS.ErrnoException, data: string) => {
-            data = data.replace('{{servers.url}}', req.protocol + '://' + req.headers.host);
-            res.send(data);
-        });
-    }
+	/**
+	 * Handle requests to /doc
+	 */
+	getDoc(req: Request, res: Response) {
+		fs.readFile(
+			'openapi.yaml',
+			'utf8',
+			(err: NodeJS.ErrnoException, data: string) => {
+				data = data.replace(
+					'{{servers.url}}',
+					req.protocol + '://' + req.headers.host
+				);
+				res.send(data);
+			}
+		);
+	}
 
-    /**
-     * Handle requests to /_version
-     */
-    getVersion(req: Request, res: Response) {
-        fs.readFile('_version', 'utf8', (err: NodeJS.ErrnoException, data: string) => {
-            res.send(err ? 'latest' : data);
-        });
-    }
+	/**
+	 * Handle requests to /_version
+	 */
+	getVersion(req: Request, res: Response) {
+		fs.readFile(
+			'_version',
+			'utf8',
+			(err: NodeJS.ErrnoException, data: string) => {
+				res.send(err ? 'latest' : data);
+			}
+		);
+	}
 
-    /**
-     * Create new HTTP controller
-     */
-    create(): express.Express {
-        const app = express();
-        app.use(this.shutdownMiddleware);
-        app.use(express.urlencoded({ extended: true, limit: '10mb' }), express.json());
+	/**
+	 * Create new HTTP controller
+	 */
+	create(): express.Express {
+		const app = express();
         // CORS
-        app.use(this.corsMiddleware);
-        // Share files from public folder
-        app.use(express.static('public'));
-        // Root
-        app.get('/', this.getRoot);
-        // Documentation
-        app.get('/doc', this.getDoc);
-        // Version
-        app.get('/_version', this.getVersion);
+		app.use(
+			cors({
+				origin: '*',
+			})
+		);
+        app.use(express.json());
 
-        //app.use(this.apiKeyMiddleware(Config.api_key));
+		app.use(
+			express.urlencoded({ extended: true, limit: '10mb' }),
+			express.json()
+		);
+		
+		// Share files from public folder
+		app.use(express.static('public'));
+		// Root
+		app.get('/', this.getRoot);
+		// Documentation
+		app.get('/doc', this.getDoc);
+		// Version
+		app.get('/_version', this.getVersion);
 
-        // routes
-        app.get("/api", (req, res) => {
-            res.json({ message: "Hello from server!" });
-        });
-        // routes
-        new Routes().route(app);
+		//app.use(this.apiKeyMiddleware(Config.api_key));
 
-        return app;
-    }
+		// routes
+		app.get('/api', (req, res) => {
+			res.json({ message: 'Hello from server!' });
+		});
+		// routes
+		new Routes().route(app);
+
+		return app;
+	}
 }
